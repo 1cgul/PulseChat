c3d106fc8a816931c9aaa91390917f92f34de693
isaiah compere <rewrittenzip@gmail.com>
2023-11-10 16:30:44 -0500

    added main screen

--- src/main/java/com/example/hackathon/MainScreenController.java
+++ src/main/java/com/example/hackathon/MainScreenController.java
@@ -1,5 +1,25 @@
 package com.example.hackathon;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class MainScreenController {
+
+    @FXML
+    private TextArea chatBox;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    private TextField tf_msg;
+
+    @FXML
+    void sendMessage(ActionEvent event) {
+
+    }
 
 }
--- src/main/java/com/example/hackathon/websocket/Websocket.java
+++ src/main/java/com/example/hackathon/websocket/Websocket.java
@@ -2,40 +2,12 @@ package com.example.hackathon.websocket;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @ServerEndpoint("/chat")
 public class Websocket {
-    private static Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
-    @OnOpen
-    public void onOpen(Session session) {
-        sessions.add(session);
-        sendMessageToAll("User connected: " + session.getId());
-    }
     @OnMessage
-    public void onMessage(String message, Session session) {
-        sendMessageToAll(session.getId() + ": " + message);
-    }
-
-    @OnClose
-    public void onClose(Session session) {
-        sessions.remove(session);
-        sendMessageToAll("User disconnected: " + session.getId());
-    }
-    @OnError
-    public void onError(Throwable t) {
-        t.printStackTrace();
-    }
-    private void sendMessageToAll(String message) {
-        for (Session session : sessions) {
-            try {
-                session.getBasicRemote().sendText(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public String handleMessage(String message) {
+        System.out.println("New Text Message Received");
+        return message;
     }
 }
