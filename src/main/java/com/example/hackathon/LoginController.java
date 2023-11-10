1e1967300f4137b0fc565022f8182359b39d8517
Christopher Gulino <123916146+1cgul@users.noreply.github.com>
2023-11-10 15:40:45 -0500

    chat button works

--- src/main/java/com/example/hackathon/LoginController.java
+++ src/main/java/com/example/hackathon/LoginController.java
@@ -2,9 +2,15 @@ package com.example.hackathon;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -13,8 +19,13 @@ public class LoginController {
     private TextField tf_username;
 
     @FXML
-    protected void onChatButtonClick() {
-
+    protected void onChatButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene.getStylesheets().add(getClass().getResource("/styling/dracula.css").toExternalForm());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
